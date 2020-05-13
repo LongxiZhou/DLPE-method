1c97e4c3a6400c81ef83b56ec412c1536336b7d7
LongxiZhou <50166538+LongxiZhou@users.noreply.github.com>
2020-05-13 15:26:51 +0300

    Update pre_process.py

--- 03.baselines.demo/3D V-net/pre_process.py
+++ 03.baselines.demo/3D V-net/pre_process.py
@@ -13,8 +13,8 @@ test_patient_list =  os.listdir(main_data_path)
 for i in test_patient_list:
     print(i)
     sub = np.load(main_data_path + i)
-    sub_raw_data = sub[:,:,:,0]
-    sub_label = sub[:,:,:,1]
+    sub_raw_data = sub[:,:,:]
+    sub_label = sub[:,:,:] #  we do not have label, use this to hold place but the model will not use it
     sub_raw_data = (sub_raw_data - np.min(sub_raw_data))/(np.max(sub_raw_data) - np.min(sub_raw_data))
     train= h5py.File(destin_path+i[0:-4]+'.h5','w')
     train.create_dataset('raw',data = sub_raw_data)
