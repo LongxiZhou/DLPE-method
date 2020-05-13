7dfddbcf68e1e305e737b4c3a70cc1d7d8129363
LongxiZhou <50166538+LongxiZhou@users.noreply.github.com>
2020-05-13 15:51:36 +0300

    Update pre_process.py

--- 03.baselines.demo/3D V-net/pre_process.py
+++ 03.baselines.demo/3D V-net/pre_process.py
@@ -5,8 +5,8 @@ import numpy as np
 from PIL import Image
 
 
-main_data_path = '/ibex/scratch/projects/c2052/haoyang_raw/li/test/'
-destin_path='/ibex/scratch/projects/c2052/haoyang_raw/li/test/'
+main_data_path = '/home/zhoul0a/Desktop/COVID-19/reports/COVID-19-repo-master/03.baselines.demo/3D V-net/arrays_raw/'
+destin_path='/home/zhoul0a/Desktop/COVID-19/reports/COVID-19-repo-master/03.baselines.demo/3D V-net/h5files/'
 
 test_patient_list =  os.listdir(main_data_path)
 
@@ -14,7 +14,7 @@ for i in test_patient_list:
     print(i)
     sub = np.load(main_data_path + i)
     sub_raw_data = sub[:,:,:]
-    sub_label = sub[:,:,:] #  we do not have label, use this to hold place but the model will not use it
+    sub_label = sub[:,:,:] #  we do not have labels, use this to hold place but the model will not use it
     sub_raw_data = (sub_raw_data - np.min(sub_raw_data))/(np.max(sub_raw_data) - np.min(sub_raw_data))
     train= h5py.File(destin_path+i[0:-4]+'.h5','w')
     train.create_dataset('raw',data = sub_raw_data)
