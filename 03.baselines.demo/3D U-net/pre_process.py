c82ceff193051fd29bea8108b643ead12ed9abd9
leihouyeung <501751638@qq.com>
2020-05-22 22:42:26 +0800

    Add files via upload

--- 03.baselines.demo/3D U-net/pre_process.py
+++ 03.baselines.demo/3D U-net/pre_process.py
@@ -3,19 +3,22 @@ import shutil
 import h5py
 import numpy as np
 from PIL import Image
-import Functions
 
-main_data_path = Functions.get_current_dict() + '/arrays_raw/'
-destin_path=Functions.get_current_dict() + '/h5files/'
+
+main_data_path = './arrays_raw/'
+destin_path='./pytorch3dunet/data/'
 
 test_patient_list =  os.listdir(main_data_path)
 
 for i in test_patient_list:
-
-	sub = np.load(main_data_path + i)
-	sub_raw_data = sub[:,:,:]
-	sub_label = sub[:,:,:] # there is no label when testing
-	train= h5py.File(destin_path+i[0:-4]+'.h5','w')
-	train.create_dataset('raw',data = sub_raw_data)
-	train.create_dataset('label',data = sub_label)
-	train.close()
+    print(i)
+    train = h5py.File(destin_path + i[0:-4] + '.h5', 'w')
+    sub = np.load(main_data_path + i)
+    if sub.ndim == 3:
+        train.create_dataset('raw', data=sub)
+    if sub.ndim == 4:
+        sub_raw_data = sub[:,:,:,0]
+        sub_label = sub[:,:,:,1]
+        train.create_dataset('raw',data = sub_raw_data)
+        train.create_dataset('label',data = sub_label)
+    train.close()
