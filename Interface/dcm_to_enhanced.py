7ba6f6710bd9a5ab0c7a40c9cfa16d8aa927593d
LongxiZhou <50166538+LongxiZhou@users.noreply.github.com>
2022-05-22 18:31:42 +0300

    Update dcm_to_enhanced.py

--- Interface/dcm_to_enhanced.py
+++ Interface/dcm_to_enhanced.py
@@ -3,11 +3,12 @@ import prediction.predict_rescaled as predictor
 import Format_convert.dcm_np_converter as normalize
 import Interface.visualize_example as visualize
 import Tool_Functions.Functions as Functions
-import torch
 
 """
 each chest CT needs about one minute to be enhanced on one V100 GPU.
 """
+
+batch_size = 4  # Need about 3 GB GPU Ram
 
 trained_model_top_dict = '/home/zhoul0a/Desktop/outside_share/trained_models/'
 # the directory where the trained models are saved
@@ -23,18 +24,18 @@ predictor.top_directory_check_point = trained_model_top_dict
 rescaled_ct_array = normalize.dcm_to_spatial_signal_rescaled(dcm_directory, wc_ww=(-600, 1600))
 # you can set wc_ww to None if dcm or dicom contains information about correct lung window.
 
-lung_mask = predictor.predict_lung_masks_rescaled_array(rescaled_ct_array, refine=False)
+lung_mask = predictor.predict_lung_masks_rescaled_array(rescaled_ct_array, refine=False, batch_size=batch_size)
 
 airway_mask = predictor.get_prediction_airway(rescaled_ct_array, lung_mask=lung_mask, semantic_ratio=0.02,
-                                              refine_airway=False)
+                                              refine_airway=False, batch_size=batch_size)
 # refine_airway will leave one connected component (discard around 1% predicted positives) and cost about 30 sec.
 
 blood_vessel_mask = predictor.get_prediction_blood_vessel(rescaled_ct_array, lung_mask=lung_mask, semantic_ratio=0.1,
-                                                          refine_blood_vessel=False)
+                                                          refine_blood_vessel=False, batch_size=batch_size)
 # refine_blood_vessel will leave one connected component (discard around 2% predicted positives) and cost about 60 sec.
 
-DLPE_enhanced, w_l, w_w = enhancement.remove_airway_and_blood_vessel_general_sampling(rescaled_ct_array, lung_mask, airway_mask,
-                                                                            blood_vessel_mask, window=True)
+DLPE_enhanced, w_l, w_w = enhancement.remove_airway_and_blood_vessel_general_sampling(rescaled_ct_array, lung_mask,
+                                                                                      airway_mask, blood_vessel_mask, window=True)
 w_l = round(w_l)  # the optimal window level for observing sub-visual parenchyma lesions
 w_w = round(w_w)  # the window width for observing sub-visual parenchyma lesions
 print("\n\n#############################################")
@@ -58,10 +59,13 @@ import prediction.predict_enhanced as predict_enhanced
 predict_enhanced.top_directory_check_point = trained_model_top_dict
 
 # mask for visible and sub-visual COVID-19 lesions for FOLLOW UP patients
-follow_up_lesions = predict_enhanced.get_invisible_covid_19_lesion_from_enhanced(DLPE_enhanced, follow_up=True)
+follow_up_lesions = predict_enhanced.get_invisible_covid_19_lesion_from_enhanced(
+    DLPE_enhanced, follow_up=True, batch_size=batch_size)
 
 # mask for visible and sub-visual COVID-19 lesions for FOLLOW UP patients
-inpatient_lesions = predict_enhanced.get_invisible_covid_19_lesion_from_enhanced(DLPE_enhanced, follow_up=False)
+inpatient_lesions = predict_enhanced.get_invisible_covid_19_lesion_from_enhanced(DLPE_enhanced, follow_up=False,
+                                                                                 batch_size=batch_size)
 
 # mask for visible COVID-19 lesions described in our TMI paper DOI: 10.1109/TMI.2020.3001810
-visible_lesions = predictor.predict_covid_19_infection_rescaled_array(rescaled_ct_array, lung_mask=lung_mask)
+visible_lesions = predictor.predict_covid_19_infection_rescaled_array(rescaled_ct_array, lung_mask=lung_mask,
+                                                                      batch_size=batch_size)
